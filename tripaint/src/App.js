5fa236e28fe0684a2730acea25e63eb06edda388
Yagiz Can Aslan <can.aslan@ug.bilkent.edu.tr>
2023-10-03 16:38:16 +0300

    WebGL commit

--- tripaint/src/App.js
+++ tripaint/src/App.js
@@ -7,12 +7,14 @@ import './styles/App.css';
 function App() {
   return (
     <div className="App">
-      <div classname="header_container">
+      <div className="header_container">
         <div className="t_header"><Header /></div>
       </div>
       <div className="content_container">
         <div className="t_leftui"><LeftUI /></div>
-        <div className="t_canvas"><canvas /></div>
+        <div className="t_canvas">
+          <canvas id="gl_canvas">Your browser doesn't support the HTML5 canvas element!</canvas>
+        </div>
         <div className="t_rightui"><RightUI /></div>
       </div>
       <div className="footer_container">
