778e1eff50a255c4aa8a5bf8b12f8814d19e02f0
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2023-10-14 21:57:32 +0300

    Updated UI style

--- triangle.js
+++ triangle.js
@@ -1,55 +1,93 @@
-/* eslint-disable no-undef */
-
 var gl;
+var canvas;
 var points;
+var view;
+var projection;
+var program;
+
+var zoom = 1.0;
+var panX = 0.0;
+var panY = 0.0;
+
+var mouseDown = false;
+var lastMouseX;
+var lastMouseY;
+
+// Add mouse event listeners
+function moveCanvasMouseDown (event) {
+    mouseDown = true;
+    lastMouseX = event.clientX;
+    lastMouseY = event.clientY;
+}
+
+function moveCanvasMouseDrag (event) {
+    // if mouse is not down, don't do anything
+    if (!mouseDown) {
+        return;
+    }
+
+    // find coordinate differences
+    var deltaX = event.clientX - lastMouseX;
+    var deltaY = event.clientY - lastMouseY;
+
+    lastMouseX = event.clientX;
+    lastMouseY = event.clientY;
+
+    // Update the pan based on mouse drag
+    panX += deltaX / (canvas.width / 2 / zoom);
+    panY -= deltaY / (canvas.height / 2 / zoom);
+
+    render();
+}
+
+function moveCanvasMouseUp (event) {
+    mouseDown = false;
+}
 
 window.onload = function init() {
-    var canvas = document.getElementById( "gl_canvas" );
+    canvas = document.getElementById( "gl_canvas" );
     gl = WebGLUtils.setupWebGL( canvas );    
     if ( !gl ) { alert( "WebGL isn't available" ); }        
+
+    // Three Vertices        
 
     var vertices = [
         vec2( -1, -1 ),
         vec2(  0,  1 ),
         vec2(  1, -1 )    
-    ];    
+    ];  
+    
+    canvas.addEventListener("mousedown", moveCanvasMouseDown);
+    canvas.addEventListener("mousemove", moveCanvasMouseDrag);
+    canvas.addEventListener("mouseup", moveCanvasMouseUp);
 
-    let points = [][30]; 
-    for (let i = 0; i <= 30; i++) {
-        for (let j = 0; j <= 30; j++) {
-            points.push(vec2(-1 + j/15, -1 + i/15));
-        }
-    }
-
-    var vertices1 = [
-        points[0],
-        points[90],
-        points[40]    
-    ];
-
-    //  Configure WebGL 
+    //  Configure WebGL   
+    //    
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );   
      
     //  Load shaders and initialize attribute buffers
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );        
 
     // Load the data into the GPU        
+
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices1), gl.STATIC_DRAW ); 
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW ); 
 	
     // Associate out shader variables with our data buffer
+
     var vPosition = gl.getAttribLocation( program, "vPosition" );
-
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition ); 
-
+    gl.enableVertexAttribArray( vPosition );    
     render();
 };
 
 function render() {
+    console.log("bitches this is render")
     gl.clear( gl.COLOR_BUFFER_BIT ); 
+
     gl.drawArrays( gl.TRIANGLES, 0, 3 );
 }
